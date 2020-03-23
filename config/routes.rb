5562f7d85bd98483457926ccb766cb00a048c17a
Junyeol_Lee <alltheway544@gmail.com>
2020-03-23 15:09:55 -0500

    added scss

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root to: 'customers#index'
+  get '/customers/missing_email', to: 'customers#missing_email'
+  get '/customers/alphabetized', to: 'customers#alphabetized'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
