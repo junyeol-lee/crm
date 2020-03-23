00d5a3cd81cfbe7b99be5a2c5c1196ed62a0600d
Junyeol_Lee <alltheway544@gmail.com>
2020-03-23 12:31:46 -0500

    active storage added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'customers#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'customers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
